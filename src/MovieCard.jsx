a06dfd65549a9947c2f871e7f6097150f53f0bf6
balajiinfo8 <vinothkumarbalaji150@gmail.com>
2025-08-24 20:03:43 +0530

    updated new feature payment option clone

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -2,13 +2,20 @@ import PropTypes from 'prop-types';
 
 function MovieCard(props)
 {
+
+    function  applayDiscount(discount,e)
+    {
+        console.log(props.title,"purchased with",discount,"% price discount");
+        console.log(e);
+    }
     return(
         <div className='movie'>
             <img src={props.image} alt={props.name} />
             <h3>{props.title}</h3>
             <h2>{props.price}</h2>
             <h1>{props.payment}</h1>
-            <span>{props.rating}</span>
+            <span>{props.rating}</span><br />
+            <button onClick={(event) => applayDiscount(20,event)}>Buy Now</button>
         </div>
     );
 }
