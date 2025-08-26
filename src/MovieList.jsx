1d1dc7d6148625c9e35f41d8784cec751d8067aa
balajiinfo8 <vinothkumarbalaji150@gmail.com>
2025-08-26 15:55:52 +0530

    now included created and read in the react.js website with using json data

--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -3,7 +3,56 @@ import 'typeface-roboto';
 import { useEffect, useState } from 'react';
 function MovieList(){
 
-    const [movies , setMovies] = useState(null);
+    const [movies , setMovies] = useState([
+    {
+        "id": "1",
+        "image": "http://localhost:3000/assets/Musk.jpg",
+        "title": "Iron Musk",
+        "price": 400,
+        "payment": "$ or ₹",
+        "rating": "5.4⭐/5.5⭐"
+    },
+    {
+        "id": "2",
+        "image": "http://localhost:3000/assets/Hacking.jpg",
+        "title": "Ethical World",
+        "price": 300,
+        "payment": "$ or ₹",
+        "rating": "5.5⭐/5.5⭐"
+    },
+    {
+        "id": "3",
+        "image": "http://localhost:3000/assets/Pursuit.jpg",
+        "title": "pursuit of happyness",
+        "price": 400,
+        "payment": "$ or ₹",
+        "rating": "5.0⭐/5.5⭐"
+    },
+    {
+        "id": "4",
+        "image": "http://localhost:3000/assets/IronMan.jpg",
+        "title": "Iron Man",
+        "price": 400,
+        "payment": "$ or ₹",
+        "rating": "5.5⭐/5.5⭐"
+    },
+    {
+        "id": "5",
+        "image": "http://localhost:3000/assets/Iron Man.jpg",
+        "title": "Iron Man 2",
+        "price": 500,
+        "payment": "$ or ₹",
+        "rating": "5.5⭐/5.5⭐"
+    },
+    {
+        "id": "8",
+        "image": "http://localhost:3000/assets/whoami.jpg",
+        "title": "WhoAmI",
+        "price": 10000,
+        "payment": "$ or ₹",
+        "rating": "5.5⭐/5.5⭐"
+    }
+]);
     
 // sort 
     // movies.sort((x,y) => x.price - y.price)
@@ -14,7 +63,7 @@ function MovieList(){
         console.log(movies);
 
         // get the response 
-        fetch("http://localhost:3000/movies")
+        fetch("https://jsonplaceholder.typicode.com/posts")
         // return the response 
         .then(response => {
             console.log(response);
