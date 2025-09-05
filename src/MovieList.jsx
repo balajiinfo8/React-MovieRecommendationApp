b3ea3b76e07dc7e0c931c54aba7a455c78b45058
balajiinfo8 <vinothkumarbalaji150@gmail.com>
2025-09-05 19:18:58 +0530

    updated imagaes

--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -1,78 +1,62 @@
 import MovieCard from './MovieCard';
 import 'typeface-roboto';
 import { useEffect, useState } from 'react';
-function MovieList(){
 
-    const [movies , setMovies] = useState(null);
-    
-// sort 
-    // movies.sort((x,y) => x.price - y.price)
+function MovieList() {
+  const [movies, setMovies] = useState(null);
+  const [error, setError] = useState(null);
 
-    const [error,setError] = useState(null);
+  useEffect(() => {
+    console.log("Use Effect Called");
 
-    useEffect(() => {
-        console.log("Use Effecct Called");
-        console.log(movies);
+    fetch("https://api.jsonsilo.com/public/31707bac-e12c-420d-99f5-3c6f57d6a25f")
+      .then(response => {
+        if (!response.ok) throw new Error("Couldn't fetch data");
+        return response.json();
+      })
+      .then(data => {
+        if (Array.isArray(data.movies)) {
+          setMovies(data.movies);
+        } else {
+          throw new Error("Invalid Data Format");
+        }
+      })
+      .catch(err => setError(err.message));
+  }, []);
 
+  function handleDelete(id) {
+    const newMovie = movies.filter(movie => movie.id !== id);
+    setMovies(newMovie);
+  }
 
-        // set time out in the fetch
-        setTimeout(()=>{
-        // get the response 
-        fetch("http://localhost:3000/movies")
-        // return the response 
-        .then(response => {
-            if(!response.ok) {
-                throw Error("Coudn't able to throw erros");
-            }
-            console.log(response);
-            return response.json();
-        })
-        .then(data => setMovies(data))
-        .catch((error)=>{
-            console.log(error.message);
-            setError(error.message);
-        })},1000)
-    },[])
-
-
-    function handleDelete(id) {
-        const newMovie = movies.filter((movies) => movies.id != id);
-        setMovies(newMovie);
-    }
-
-
-    if(!movies){
-        return (
-            <>
-            {!error && <img src="data/assets/loading.gif"></img>}
-            {error && <p>{error}</p>}
-            </>
-        )
-    }
-
-
-
-    const movieList = movies.map(
-        movie =>(
-             <MovieCard
-                key={movie.id}  
-                title={movie.title}
-                price={movie.price} 
-                payment={movie.payment}
-                rating={movie.rating}
-                image={movie.image}
-                delete={handleDelete}
-                id={movie.id}  
-             />
-            ))
-
+  if (!movies) {
     return (
-       <>
-        
-        <h1 style={{textAlign: 'center' , fontFamily : 'sans-serif'}}>Netflix Clone</h1>
-         {movieList}
-       </>
-    );  
+      <>
+        {!error && <img src="data/assets/loading.gif" alt="loading" />}
+        {error && <p>{error}</p>}
+      </>
+    );
+  }
+
+  const movieList = movies.map(movie => (
+    <MovieCard
+      key={movie.id}
+      title={movie.title}
+      price={movie.price}
+      payment={movie.payment}
+      rating={movie.rating}
+      image={movie.image}
+      delete={handleDelete}
+      id={movie.id}
+    />
+  ));
+
+  return (
+    <>
+      <h1 style={{ textAlign: 'center', fontFamily: 'sans-serif' }}>Netflix Clone</h1>
+      {movieList}
+    </>
+  );
 }
 
 export default MovieList;
